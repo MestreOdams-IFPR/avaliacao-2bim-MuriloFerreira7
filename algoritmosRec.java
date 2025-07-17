2363ac30c123f8460211d3c5d1114e10ddff7a2d
OdairMoreira <odair.moreira.souza>
2025-07-17 15:35:51 -0300

    Conceito A | Prof. Odair

--- algoritmosRec.java
+++ algoritmosRec.java
@@ -2,9 +2,10 @@ import java.util.Scanner;
 
 public class algoritmosRec {
     final static Scanner LER = new Scanner(System.in);
+
     public static void main(String[] args) {
         // System.out.println(tribonacci(LER.nextInt()));
-        System.out.println(pell(LER.nextInt()));    
+        System.out.println(pell(LER.nextInt()));
         // int[] nsei = {1, 2, 3, 4, 5, 6, 7, 8};
         // metodoRec02(nsei, 0, 7);
     }
@@ -13,20 +14,19 @@ public class algoritmosRec {
         if (n == 0 || n == 1) {
             return n;
         }
-        return 2*pell(n-1)+pell(n-2);
+        return 2 * pell(n - 1) + pell(n - 2);
     }
 
     public static int tribonacci(int n) {
         if (n == 1 || n == 0) {
             return 0;
-        } else if(n == 2) {
+        } else if (n == 2) {
             return 1;
         }
-        return tribonacci(n-1)+tribonacci(n-2)+tribonacci(n-3);
+        return tribonacci(n - 1) + tribonacci(n - 2) + tribonacci(n - 3);
     }
 
-    public static void metodoRec02(
-            int v[], int e, int d) {
+    public static void metodoRec02(int v[], int e, int d) {
         int t;
         if (e >= d)
             return;
